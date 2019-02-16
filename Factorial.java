5d94d5684c7e318c8328f1bbec5717e09ee3ac6d
[dinithi] <[dinithimweerasinghe95@gmail.com>
2019-02-16 23:49:38 +0530

    factorial
    factorial

---
+++ Factorial.java
@@ -0,0 +1,14 @@
+
+public class Factorial {
+    public static void main(String args[]){
+      
+        int fac=1;
+        int number=5;
+        
+        for(int i=1;i<number;i++){
+        fac=fac*i;
+        }
+        System.out.println(fac);
+    }
+    
+}
